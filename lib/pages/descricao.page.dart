264608205def4a6bfbb9a3c21d14c0a74d098113
thiago <thiagoalbino2000@hotmail.com>
2023-08-27 20:35:20 -0300

    Verificando o funcionamento da tela

--- lib/pages/descricao.page.dart
+++ lib/pages/descricao.page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dev_movel_me_ajuda_ape/classes/imovel.dart';
 
+// ignore: must_be_immutable
 class DescricaoScreen extends StatelessWidget {
   late Imovel imovel;
   DescricaoScreen({super.key});
@@ -90,13 +91,6 @@ class DescricaoScreen extends StatelessWidget {
                     subtitle: 'Quartos',
                   ),
                 ),
-                // Expanded(
-                //   child: _buildColumnCard(
-                //     icon: Icons.bathtub_outlined,
-                //     title: '${imovel.}',
-                //     subtitle: 'Suítes',
-                //   ),
-                // ),
                 Expanded(
                   child: _buildColumnCard(
                     icon: Icons.shower_outlined,
@@ -187,20 +181,23 @@ class DescricaoScreen extends StatelessWidget {
                   ),
                   const Row(
                     children: [
-                      ContactText('Whatsapp: (12) 91234-5678'),
-                      ContactText('Código do imóvel: 12345'),
+                      FeatureText('Whatsapp: (12) 91234-5678'),
+                      FeatureText('Código do imóvel: 12345'),
                     ],
                   ),
                   Center(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF353535),
-                      ),
-                      onPressed: () {},
-                      child: const Text(
-                        'Contato',
-                        style: TextStyle(color: Color(0xFFF2BC1B)),
-                      ),
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 16),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF353535),
+                            padding: const EdgeInsets.all(10),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            'Contato',
+                            style: TextStyle(color: Color(0xFFF2BC1B)),
+                          )),
                     ),
                   ),
                 ],
@@ -211,35 +208,35 @@ class DescricaoScreen extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget _buildColumnCard({
-    required IconData icon,
-    required String title,
-    required String subtitle,
-  }) {
-    return Card(
-      color: const Color(0xFFF9F9F9),
-      elevation: 8,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      margin: const EdgeInsets.all(8),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Icon(icon, size: 30, color: const Color(0xFFF2BC1B)),
-            const SizedBox(height: 4),
-            Text(
-              title,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+Widget _buildColumnCard({
+  required IconData icon,
+  required String title,
+  required String subtitle,
+}) {
+  return Card(
+    color: const Color(0xFFF9F9F9),
+    elevation: 8,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+    margin: const EdgeInsets.all(8),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Icon(icon, size: 30, color: const Color(0xFFF2BC1B)),
+          const SizedBox(height: 4),
+          Text(
+            title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
             ),
-            Text(subtitle),
-          ],
-        ),
+          ),
+          Text(subtitle),
+        ],
       ),
-    );
-  }
+    ),
+  );
 }
 
 class FeatureText extends StatelessWidget {
@@ -255,22 +252,6 @@ class FeatureText extends StatelessWidget {
         style: const TextStyle(
           fontSize: 13,
         ),
-      ),
-    );
-  }
-}
-
-class ContactText extends StatelessWidget {
-  final String text;
-
-  const ContactText(this.text, {Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Text(
-        text,
-        style: const TextStyle(fontSize: 13),
       ),
     );
   }
@@ -295,11 +276,11 @@ class CarouselIndicator extends State<Carousel> {
   Widget build(BuildContext context) {
     return Column(children: [
       CarouselSlider(
-        items: widget.imagePaths.map((imagePath) {
+        items: widget.imagePaths.map((imageLink) {
           return Builder(
             builder: (BuildContext context) {
-              return Image.asset(
-                imagePath,
+              return Image.network(
+                imageLink,
                 width: MediaQuery.of(context).size.width,
                 height: 230,
                 fit: BoxFit.cover,
